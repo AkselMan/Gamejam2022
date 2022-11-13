7be4cd5a3df732064117832bf9d1de6b5e3676fe
tobtob <83303981+tobtob-hax@users.noreply.github.com>
2022-11-13 08:23:41 +0100

    bam

--- Assets/DeathTrigger.cs
+++ Assets/DeathTrigger.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathTrigger : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D collision)
     {
         print("DEAD");
+        SceneManager.LoadScene("DeathMenu");
     }
 }
