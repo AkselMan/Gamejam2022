c7f6b1f31423b3ea0aca7f8fb9d57d0a4e77be61
AkselMan <45812757+AkselMan@users.noreply.github.com>
2022-11-13 01:01:41 +0100

    Death Trigger

--- Assets/DeathTrigger.cs
+++ Assets/DeathTrigger.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class DeathTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        print("DEAD");
     }
 }
